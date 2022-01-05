10e648c236ee601b0aabd6a9be2a536904a46a19
dhiabh <meddhia.bh99@gmail.com>
2022-01-05 16:37:38 +0100

    first commit

---
+++ E-commerce-ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_commerce_ASP.MvcApplication" Language="C#" %>
---
+++ E-commerce-ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_commerce_ASP.Startup))]
+namespace E_commerce_ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

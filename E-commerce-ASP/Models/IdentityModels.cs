523a4c8f5044e2a127ef362460a790341bae58b0
Elyes9918 <elyes9918@gmail.com>
2022-01-09 13:10:13 +0100

    Fixed Application user Erorr

--- E-commerce-ASP/Models/IdentityModels.cs
+++ E-commerce-ASP/Models/IdentityModels.cs
@@ -65,6 +65,6 @@ namespace E_commerce_ASP.Models
             return new ApplicationDbContext();
         }
 
-        public System.Data.Entity.DbSet<E_commerce_ASP.Models.ApplicationUser> ApplicationUsers { get; set; }
+        //public System.Data.Entity.DbSet<E_commerce_ASP.Models.ApplicationUser> ApplicationUsers { get; set; }
     }
 }

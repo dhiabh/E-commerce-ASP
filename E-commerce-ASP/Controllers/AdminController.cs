5243bcb49506a961e302e84d6ed21c4d059faac0
Elyes9918 <elyes9918@gmail.com>
2022-01-10 13:14:33 +0100

    Added Admin Dashboard

--- E-commerce-ASP/Controllers/AdminController.cs
+++ E-commerce-ASP/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_commerce_ASP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,16 @@ namespace E_commerce_ASP.Controllers
     [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
+
+        private Entities db = new Entities();
+
         // GET: Admin
         public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult DashBoard()
         {
             return View();
         }
@@ -20,7 +29,23 @@ namespace E_commerce_ASP.Controllers
             return View();
         }
 
-        public ActionResult Charts()
+        public ActionResult Historique()
+        {
+            return View();
+        }
+
+        public ActionResult DetailUtilisateur()
+        {
+            return View();
+        }
+
+
+        public ActionResult ListeNoire()
+        {
+            return View();
+        }
+
+        public ActionResult ListeFavoris()
         {
             return View();
         }
